b848d8ab24f2b4ae6483a7ef211930c3479146d7
pradyot <pradyotprksh4@gmail.com>
2019-10-05 22:46:57 +0530

    Refact: review code refactor and ui

--- app/src/main/java/com/project/pradyotprakash/polking/profile/reviewUs/ReviewUsBtmSheet.kt
+++ app/src/main/java/com/project/pradyotprakash/polking/profile/reviewUs/ReviewUsBtmSheet.kt
@@ -2,18 +2,14 @@ package com.project.pradyotprakash.polking.profile.reviewUs
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.project.pradyotprakash.polking.R
 import com.project.pradyotprakash.polking.profileDetails.ProfileEditView
-import com.project.pradyotprakash.polking.utility.RoundBottomSheet
+import com.project.pradyotprakash.polking.utility.TransparentBottomSheet
 import com.project.pradyotprakash.polking.utility.Utility
 import com.project.pradyotprakash.polking.utility.logd
 import dagger.android.AndroidInjection
@@ -24,7 +20,7 @@ import java.util.*
 import javax.inject.Inject
 import kotlin.collections.HashMap
 
-class ReviewUsBtmSheet @Inject constructor() : RoundBottomSheet(), ProfileEditView {
+class ReviewUsBtmSheet @Inject constructor() : TransparentBottomSheet(), ProfileEditView {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
@@ -44,14 +40,6 @@ class ReviewUsBtmSheet @Inject constructor() : RoundBottomSheet(), ProfileEditVi
         AndroidInjection.inject(this.activity)
         val view = inflater.inflate(R.layout.review_us_btm_sheet, container, false)
 
-        dialog!!.setOnShowListener { dialog ->
-            val bottomSheetDialog: BottomSheetDialog = dialog as BottomSheetDialog
-            val bottomSheetInternal = bottomSheetDialog.findViewById<FrameLayout>(R.id.design_bottom_sheet)
-            if (bottomSheetInternal != null) {
-                BottomSheetBehavior.from<View>(bottomSheetInternal).state = BottomSheetBehavior.STATE_EXPANDED
-            }
-        }
-
         activity!!.logd(getString(R.string.reviewbottomsheet))
 
         initView(view)
@@ -60,196 +48,40 @@ class ReviewUsBtmSheet @Inject constructor() : RoundBottomSheet(), ProfileEditVi
     }
 
     private fun initView(view: View) {
-        mAuth = FirebaseAuth.getInstance()
-        firestore = FirebaseFirestore.getInstance()
 
+        initVariables()
+
+        setOnClickListners(view)
+    }
+
+    private fun setOnClickListners(view: View) {
         view.back_tv.setOnClickListener {
             stopAct()
         }
 
         view.feedback0_tv.setOnClickListener {
-            if (view.feedback1_tv.visibility != View.GONE) {
-                view.feedback1_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback2_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback3_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback4_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback1_tv.visibility = View.INVISIBLE
-                view.feedback2_tv.visibility = View.INVISIBLE
-                view.feedback3_tv.visibility = View.INVISIBLE
-                view.feedback4_tv.visibility = View.INVISIBLE
-                Handler().postDelayed({
-                    view.feedback1_tv.visibility = View.GONE
-                    view.feedback2_tv.visibility = View.GONE
-                    view.feedback3_tv.visibility = View.GONE
-                    view.feedback4_tv.visibility = View.GONE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.post_Tv)
-                    Utility().expandCollapse(view.val_tv)
-                    view.val_tv.text = getString(R.string.its_making_us_sad)
-                }, 300)
-            } else {
-                view.feedback1_tv.visibility = View.VISIBLE
-                view.feedback2_tv.visibility = View.VISIBLE
-                view.feedback3_tv.visibility = View.VISIBLE
-                view.feedback4_tv.visibility = View.VISIBLE
-                view.feedback1_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback2_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback3_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback4_tv.startAnimation(Utility().inFromLeftAnimation())
-
-                Utility().expandCollapse(view.addFeed_et3)
-                Utility().expandCollapse(view.val_tv)
-                Utility().expandCollapse(view.post_Tv)
-            }
+            showReviewSections(view)
+            view.val_tv.text = getString(R.string.its_making_us_sad)
         }
 
         view.feedback1_tv.setOnClickListener {
-            if (view.feedback0_tv.visibility != View.GONE) {
-                view.feedback0_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback2_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback3_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback4_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback0_tv.visibility = View.INVISIBLE
-                view.feedback2_tv.visibility = View.INVISIBLE
-                view.feedback3_tv.visibility = View.INVISIBLE
-                view.feedback4_tv.visibility = View.INVISIBLE
-                Handler().postDelayed({
-                    view.feedback0_tv.visibility = View.GONE
-                    view.feedback2_tv.visibility = View.GONE
-                    view.feedback3_tv.visibility = View.GONE
-                    view.feedback4_tv.visibility = View.GONE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.post_Tv)
-                    Utility().expandCollapse(view.val_tv)
-                    view.val_tv.text = getString(R.string.its_sad)
-                }, 300)
-            } else {
-                view.feedback0_tv.visibility = View.VISIBLE
-                view.feedback2_tv.visibility = View.VISIBLE
-                view.feedback3_tv.visibility = View.VISIBLE
-                view.feedback4_tv.visibility = View.VISIBLE
-                view.feedback0_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback2_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback3_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback4_tv.startAnimation(Utility().inFromLeftAnimation())
-
-                Utility().expandCollapse(view.addFeed_et3)
-                Utility().expandCollapse(view.val_tv)
-                Utility().expandCollapse(view.post_Tv)
-            }
+            showReviewSections(view)
+            view.val_tv.text = getString(R.string.its_sad)
         }
 
         view.feedback2_tv.setOnClickListener {
-            if (view.feedback0_tv.visibility != View.GONE) {
-                view.feedback1_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback0_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback3_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback4_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback1_tv.visibility = View.INVISIBLE
-                view.feedback0_tv.visibility = View.INVISIBLE
-                view.feedback3_tv.visibility = View.INVISIBLE
-                view.feedback4_tv.visibility = View.INVISIBLE
-                Handler().postDelayed({
-                    view.feedback1_tv.visibility = View.GONE
-                    view.feedback0_tv.visibility = View.GONE
-                    view.feedback3_tv.visibility = View.GONE
-                    view.feedback4_tv.visibility = View.GONE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.val_tv)
-                    Utility().expandCollapse(view.post_Tv)
-                    view.val_tv.text = getString(R.string.tell_us)
-                }, 300)
-            } else {
-                view.feedback1_tv.visibility = View.VISIBLE
-                view.feedback0_tv.visibility = View.VISIBLE
-                view.feedback3_tv.visibility = View.VISIBLE
-                view.feedback4_tv.visibility = View.VISIBLE
-                view.feedback1_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback0_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback3_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback4_tv.startAnimation(Utility().inFromLeftAnimation())
-
-                Utility().expandCollapse(view.addFeed_et3)
-                Utility().expandCollapse(view.val_tv)
-                Utility().expandCollapse(view.post_Tv)
-            }
+            showReviewSections(view)
+            view.val_tv.text = getString(R.string.tell_us)
         }
 
         view.feedback3_tv.setOnClickListener {
-            if (view.feedback0_tv.visibility != View.GONE) {
-                view.feedback1_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback2_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback0_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback4_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback1_tv.visibility = View.INVISIBLE
-                view.feedback2_tv.visibility = View.INVISIBLE
-                view.feedback0_tv.visibility = View.INVISIBLE
-                view.feedback4_tv.visibility = View.INVISIBLE
-                Handler().postDelayed({
-                    view.feedback1_tv.visibility = View.GONE
-                    view.feedback2_tv.visibility = View.GONE
-                    view.feedback0_tv.visibility = View.GONE
-                    view.feedback4_tv.visibility = View.GONE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.val_tv)
-                    Utility().expandCollapse(view.post_Tv)
-                    view.val_tv.text = getString(R.string.thanks)
-                }, 300)
-            } else {
-                view.feedback1_tv.visibility = View.VISIBLE
-                view.feedback2_tv.visibility = View.VISIBLE
-                view.feedback0_tv.visibility = View.VISIBLE
-                view.feedback4_tv.visibility = View.VISIBLE
-                view.feedback1_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback2_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback0_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback4_tv.startAnimation(Utility().inFromLeftAnimation())
-
-                Utility().expandCollapse(view.addFeed_et3)
-                Utility().expandCollapse(view.val_tv)
-                Utility().expandCollapse(view.post_Tv)
-            }
+            showReviewSections(view)
+            view.val_tv.text = getString(R.string.thanks)
         }
 
         view.feedback4_tv.setOnClickListener {
-            if (view.feedback0_tv.visibility != View.GONE) {
-                view.feedback1_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback2_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback3_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback0_tv.startAnimation(Utility().outToRightAnimation())
-                view.feedback1_tv.visibility = View.INVISIBLE
-                view.feedback2_tv.visibility = View.INVISIBLE
-                view.feedback3_tv.visibility = View.INVISIBLE
-                view.feedback0_tv.visibility = View.INVISIBLE
-                Handler().postDelayed({
-                    view.feedback1_tv.visibility = View.GONE
-                    view.feedback2_tv.visibility = View.GONE
-                    view.feedback3_tv.visibility = View.GONE
-                    view.feedback0_tv.visibility = View.GONE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.val_tv)
-                    Utility().expandCollapse(view.post_Tv)
-                    view.val_tv.text = getString(R.string.very_much_thanks)
-                }, 300)
-            } else {
-                view.feedback1_tv.visibility = View.VISIBLE
-                view.feedback2_tv.visibility = View.VISIBLE
-                view.feedback3_tv.visibility = View.VISIBLE
-                view.feedback0_tv.visibility = View.VISIBLE
-                view.feedback1_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback2_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback3_tv.startAnimation(Utility().inFromLeftAnimation())
-                view.feedback0_tv.startAnimation(Utility().inFromLeftAnimation())
-
-                Utility().expandCollapse(view.addFeed_et3)
-                Utility().expandCollapse(view.val_tv)
-                Utility().expandCollapse(view.post_Tv)
-            }
+            showReviewSections(view)
+            view.val_tv.text = getString(R.string.very_much_thanks)
         }
 
         view.post_Tv.setOnClickListener {
@@ -261,11 +93,16 @@ class ReviewUsBtmSheet @Inject constructor() : RoundBottomSheet(), ProfileEditVi
                 0 1 2 3 4 are the stars value
                  */
                 when {
-                    view.feedback0_tv.visibility == View.VISIBLE -> reviewData["chooseOption"] = "0"
-                    view.feedback1_tv.visibility == View.VISIBLE -> reviewData["chooseOption"] = "1"
-                    view.feedback2_tv.visibility == View.VISIBLE -> reviewData["chooseOption"] = "2"
-                    view.feedback3_tv.visibility == View.VISIBLE -> reviewData["chooseOption"] = "3"
-                    view.feedback4_tv.visibility == View.VISIBLE -> reviewData["chooseOption"] = "4"
+                    view.val_tv.text == getString(R.string.its_making_us_sad) -> reviewData["chooseOption"] =
+                        "0"
+                    view.val_tv.text == getString(R.string.its_sad) -> reviewData["chooseOption"] =
+                        "1"
+                    view.val_tv.text == getString(R.string.tell_us) -> reviewData["chooseOption"] =
+                        "2"
+                    view.val_tv.text == getString(R.string.thanks) -> reviewData["chooseOption"] =
+                        "3"
+                    view.val_tv.text == getString(R.string.very_much_thanks) -> reviewData["chooseOption"] =
+                        "4"
                 }
                 reviewData["givenOnDate"] = dateFormat.format(date)
                 reviewData["givenOnTime"] = timeFormat.format(date)
@@ -276,35 +113,52 @@ class ReviewUsBtmSheet @Inject constructor() : RoundBottomSheet(), ProfileEditVi
                     "Nothing To Say"
                 }
 
-                firestore.collection("reviews").document().set(reviewData).addOnSuccessListener {
-                    hideLoading()
-                    showMessage(
-                        "Thanks For Sharing Your Information",
-                        3
-                    )
-                    view.feedback1_tv.visibility = View.VISIBLE
-                    view.feedback2_tv.visibility = View.VISIBLE
-                    view.feedback3_tv.visibility = View.VISIBLE
-                    view.feedback0_tv.visibility = View.VISIBLE
-                    view.feedback4_tv.visibility = View.VISIBLE
-
-                    Utility().expandCollapse(view.addFeed_et3)
-                    Utility().expandCollapse(view.val_tv)
-                    Utility().expandCollapse(view.post_Tv)
-                }.addOnFailureListener { exception ->
-                    showMessage(
-                        "Something Went Wrong. ${exception.localizedMessage}",
-                        1
-                    )
-                    hideLoading()
-                }.addOnCanceledListener {
-                    showMessage(getString(R.string.not_uploaded_question), 4)
-                    hideLoading()
-                }
+                saveDataToDatabase(view, reviewData)
             } else {
                 showMessage(getString(R.string.user_not_found), 1)
             }
         }
+    }
+
+    private fun saveDataToDatabase(
+        view: View,
+        reviewData: HashMap<String, Any>
+    ) {
+        firestore.collection("reviews").document().set(reviewData).addOnSuccessListener {
+            hideLoading()
+            showMessage(
+                "Thanks For Sharing Your Information",
+                3
+            )
+
+            Utility().expandCollapse(view.addFeed_et3)
+            Utility().expandCollapse(view.val_tv)
+            Utility().expandCollapse(view.post_Tv)
+
+            Utility().hideSoftKeyboard(view.addFeed_et3)
+        }.addOnFailureListener { exception ->
+            showMessage(
+                "Something Went Wrong. ${exception.localizedMessage}",
+                1
+            )
+            hideLoading()
+        }.addOnCanceledListener {
+            showMessage(getString(R.string.not_uploaded_question), 4)
+            hideLoading()
+        }
+    }
+
+    private fun showReviewSections(view: View) {
+        if (addFeed_et3.visibility == View.GONE) {
+            Utility().expandCollapse(view.addFeed_et3)
+            Utility().expandCollapse(view.post_Tv)
+            Utility().expandCollapse(view.val_tv)
+        }
+    }
+
+    private fun initVariables() {
+        mAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
     }
 
 
